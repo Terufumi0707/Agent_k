a44d9d29b9985257a5d7e0b2458416cee7bc1228
Terufumi0707 <terufumikajinuma@gmail.com>
2025-11-01 13:58:05 +0900

    Add completion navigation prompt after workflow finishes

--- frontend/src/views/WorkspaceDashboard.vue
+++ frontend/src/views/WorkspaceDashboard.vue
@@ -7,32 +7,7 @@
 </template>
 
 <script setup>
-import { watch } from "vue";
-import { storeToRefs } from "pinia";
-import { useRouter } from "vue-router";
 import SidebarPanel from "../components/SidebarPanel.vue";
 import ChatArea from "../components/ChatArea.vue";
 import RightPane from "../components/RightPane.vue";
-import { useWorkspaceStore } from "../stores/workspace";
-
-const workspaceStore = useWorkspaceStore();
-const { activeWorkspace } = storeToRefs(workspaceStore);
-const router = useRouter();
-
-watch(
-  () => ({
-    id: activeWorkspace.value?.id,
-    status: activeWorkspace.value?.status
-  }),
-  (current, previous) => {
-    if (
-      current?.id &&
-      previous?.id === current.id &&
-      previous.status !== "completed" &&
-      current.status === "completed"
-    ) {
-      router.push({ name: "completed-workspaces" });
-    }
-  }
-);
 </script>
